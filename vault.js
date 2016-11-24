d3d08dc613615bd2ebeed31a8e23527b0c259094
jonathon.laylo@gmail.com <jonathon.laylo@gmail.com>
2016-11-23 17:02:30 -1000

    completed assignment

--- vault.js
+++ vault.js
@@ -1,4 +1,28 @@
 'use strict';
 module.exports = function() {
-  
+
+// variable storing value, key, and vault
+  var _key = 0;
+  var _value = 0;
+  var vault = {};
+
+// setting the values into valut from the key
+  function setValue(key, value){
+   vault[key] = value;
+   return vault;
+  }
+
+// if key exits return it into valut, if not return null
+  function getValue(key){
+    if(key in vault){
+      return vault[key];
+    }else{
+      return null;
+    }
+  }
+
+  return {
+    setValue: setValue,
+    getValue: getValue
+  };
 };
